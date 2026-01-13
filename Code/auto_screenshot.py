d98911847a8eaf1594e126b4d4698059fc76ea87
Prachet Singh <prachetsingh06@gmail.com>
2026-01-14 09:13:18 +1100

    Fixed multiple L2 read issue.

--- Code/auto_screenshot.py
+++ Code/auto_screenshot.py
@@ -1,13 +1,36 @@
 from pydualsense import pydualsense
-#TODO: Install the library properly
+import time
 
-# Create controller instance
+# TODO: Add logic so that times you hit the brakes in sector 1 counts and nothing else does
+#       one way to enforce this would be having a button to mark to mark the end and start of ]
+#       sector one.
+
+# TODO: Add code for screenshotting using a library that allows that to happen fast 
+
 ds = pydualsense()
-
-# Initialize connection
 ds.init()
 
-# source: https://flok.github.io/pydualsense/ds_main.html#features
-if ds.state.L2:
-    print("Brakes hit")
-    #TODO: Add screenshot mechanism and ensure that the the script is running unless manually stopped 
+count = 0
+was_pressed = False
+
+try:
+    print("Running...... (press SHARE to exit)")
+
+    while True:
+        state = ds.state
+        is_pressed = bool(state.L2)
+
+        if is_pressed and not was_pressed:
+            count += 1
+            print(f"L2 press #{count}")
+            # TODO: trigger screenshot here
+
+        was_pressed = is_pressed
+
+        if state.share:
+            print("Share pressed, exiting")
+            break
+
+        time.sleep(0.01)
+finally:
+    ds.close()
