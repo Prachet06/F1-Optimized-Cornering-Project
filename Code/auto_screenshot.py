05869281162b15fb8f2f13708f5f838b6b1265e3
Prachet Singh <prachetsingh06@gmail.com>
2026-01-14 13:48:22 +1100

    Added screenshotting mechanism, edited ReadMes, and restructured data storage.

--- Code/auto_screenshot.py
+++ Code/auto_screenshot.py
@@ -1,11 +1,35 @@
 from pydualsense import pydualsense
 import time
+import mss
+import mss.tools
 
 # TODO: Add logic so that times you hit the brakes in sector 1 counts and nothing else does
 #       one way to enforce this would be having a button to mark to mark the end and start of ]
 #       sector one.
 
-# TODO: Add code for screenshotting using a library that allows that to happen fast 
+# TODO: Ensure that the screenshots are not overwritten, maybe through the use of input each 
+#       time you run the program
+
+# TODO: Improve screenshot saves based on the inputs idea from above, so that each session's 
+#       image data is saved in a separate folder
+
+# TODO: Maybe add something to delete/ remove previous accidental screenshots
+ 
+print('''
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⣶⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⣿⠿⣿⣷⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣷⡄⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⡿⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⣿⣿⡇⠀⢸⣿⣿⣿⣿⠇⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⣀⣤ ⣿⣿⣿⣿⡇⠀⠈⠉⠉⠉⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⢀⣠⣤⣶⣾⣿⣿⡿⠀⣿⣿⣿⣿⡇⠀⢰⣶⣿⣿⣿⠿⠿⢿⣶⣦⣤⡀
+⢰⣿⣿⣿⡿⠛⠉⢀⣀⠀⣿⣿⣿⣿⡇⠀⠘⠋⠉⠀⣀⣠⣴⣾⣿⣿⣿⠇
+⠈⠻⠿⣿⣿⣿⣿⣿⠿⠀⣿⣿⣿⣿⡇⠀⢠⣶⣾⣿⣿⡿⠿⠟⠋⠉⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⠛⠿⢿⡇⠀⠸⠟⠛⠋⠁⠀⠀⠀⠀⠀⠀⠀
+''')
+print("Running...... (press SHARE to exit)")
+
 
 ds = pydualsense()
 ds.init()
@@ -14,7 +38,7 @@ count = 0
 was_pressed = False
 
 try:
-    print("Running...... (press SHARE to exit)")
+    
 
     while True:
         state = ds.state
@@ -23,7 +47,17 @@ try:
         if is_pressed and not was_pressed:
             count += 1
             print(f"L2 press #{count}")
-            # TODO: trigger screenshot here
+            # Triggering and saving screenshots below
+            with mss.mss() as sct:
+                # Get information for the laptop screen (monitor 1 at index 1)
+                monitor = sct.monitors[1] 
+                # Grab the data
+                sct_img = sct.grab(monitor)
+                # Save the picture 
+                filename = f"Break hit #{count}.png"
+                file_loc = f"../Data/Image Data/Break hit #{count}.png"
+                mss.tools.to_png(sct_img.rgb, sct_img.size, output=file_loc)
+                print(f"Screenshot saved as: {filename}")
 
         was_pressed = is_pressed
 
@@ -34,3 +68,4 @@ try:
         time.sleep(0.01)
 finally:
     ds.close()
+
