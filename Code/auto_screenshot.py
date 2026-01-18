23a9cab96b0baa18716aaea243a3be5e81a9611d
Prachet Singh <prachetsingh06@gmail.com>
2026-01-18 17:52:52 +1100

    Added auto-screenshotting for R2/ throttle and improved terminal output.

--- Code/auto_screenshot.py
+++ Code/auto_screenshot.py
@@ -14,7 +14,6 @@ import mss.tools
 
 # TODO: Maybe add something to delete/ remove previous accidental screenshots
 
-# TODO: ADD SCREENSHOT MECHANISM FOR THROTTLE TOO
  
 print('''
 ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⣶⣤⣤⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
@@ -33,17 +32,20 @@ Auto screenshot program now running......
       
 Press:
 - Triangle: Enable or disbale screenshotting 
-- L2: Take a screenshot 
+- L2: Take a screenshot when brakes are hit
+- R2: Take a screenshot when throttle is hit
 - Share: Stop the program
 ''')
 
-# Initialize Colorama (autoreset=True resets color after each print)
+# Initialize Colorama 
+# (autoreset=True resets color after each print just to be safe)
 init(autoreset=True) 
 
 ds = pydualsense()
 ds.init()
 
 was_L2_pressed = False
+was_R2_pressed = False
 was_triangle_pressed = False
 record_start = False
 
@@ -62,49 +64,86 @@ try:
             if record_start:
                 lap += 1
                 print(f"Screenshotting Enabled | Lap: {lap}")
-                print(f"{Fore.GREEN}++++++++++++++++++++++++++")
-                print(f"{Fore.GREEN}++++++++++++++++++++++++++")
-                print(f"{Fore.GREEN}++++++++++++++++++++++++++")
-                print(f"{Fore.GREEN}++++++++++++++++++++++++++")
-                print(f"{Fore.GREEN}++++++++++++++++++++++++++")
-                print(f"{Fore.GREEN}++++++++++++++++++++++++++")
-                count = 0 # For keeping track of the times the brakes were hit in each lap
+                print(f"{Fore.GREEN}++++++++++++++++++++++++      ++                    ++")
+                print(f"{Fore.GREEN}++                    ++      ++++                  ++")
+                print(f"{Fore.GREEN}++                    ++      ++  ++                ++")
+                print(f"{Fore.GREEN}++                    ++      ++    ++              ++")
+                print(f"{Fore.GREEN}++                    ++      ++      ++            ++")
+                print(f"{Fore.GREEN}++                    ++      ++        ++          ++")
+                print(f"{Fore.GREEN}++                    ++      ++          ++        ++")
+                print(f"{Fore.GREEN}++                    ++      ++            ++      ++")
+                print(f"{Fore.GREEN}++                    ++      ++              ++    ++")
+                print(f"{Fore.GREEN}++                    ++      ++                ++  ++")
+                print(f"{Fore.GREEN}++                    ++      ++                  ++++")
+                print(f"{Fore.GREEN}++++++++++++++++++++++++      ++                    ++\n")
+
+                brake_count = 0 # For keeping track of the times the brakes 
+                                # are hit when screenshotting is enabled
+                throttle_count = 0 # For keeping track of the times the the throttle
+                                   # is hit when screenshotting is enabled
             else:
                 # to avoid an unneccessary increase in lap number
-                if count == 0 and lap != 0:
+                if brake_count == 0 and lap != 0 and throttle_count == 0:
                     lap = lap - 1
-                print("Screenshotting Disabled.")
-                print(f"{Fore.RED}++++++++++++++++++++++++++")
-                print(f"{Fore.RED}++++++++++++++++++++++++++")
-                print(f"{Fore.RED}++++++++++++++++++++++++++")
-                print(f"{Fore.RED}++++++++++++++++++++++++++")
-                print(f"{Fore.RED}++++++++++++++++++++++++++")
-                print(f"{Fore.RED}++++++++++++++++++++++++++\n")
+
+                print("\nScreenshotting Disabled.")
+                print(f"{Fore.RED}------------------------      ------------------------      ------------------------")
+                print(f"{Fore.RED}------------------------      ------------------------      ------------------------")
+                print(f"{Fore.RED}--                    --      --                            --                      ")
+                print(f"{Fore.RED}--                    --      --                            --                      ")
+                print(f"{Fore.RED}--                    --      --                            --                      ")
+                print(f"{Fore.RED}--                    --      -------------                 -------------           ")
+                print(f"{Fore.RED}--                    --      -------------                 -------------           ")
+                print(f"{Fore.RED}--                    --      --                            --                      ")
+                print(f"{Fore.RED}--                    --      --                            --                      ")
+                print(f"{Fore.RED}--                    --      --                            --                      ")
+                print(f"{Fore.RED}------------------------      --                            --                      ")
+                print(f"{Fore.RED}------------------------      --                            --                      \n")
+
         was_triangle_pressed = triangle_pressed
 
         # L2 screenshot
         l2_pressed = bool(state.L2)
         if record_start and l2_pressed and not was_L2_pressed:
-            count += 1
-            print(f"L2 press #{count}")
+            brake_count += 1
+            print(f"L2 press #{brake_count}")
 
             with mss.mss() as sct:
                 monitor = sct.monitors[1]
                 sct_img = sct.grab(monitor)
 
-                file_loc = f"../Data/image-data/brake/Lap_{lap}_Brake hit_{count}.png"
-                file_name = f"Lap_{lap}_Brake hit_{count}.png"
+                file_loc = f"../Data/image-data/brake/Lap_{lap}_Brake hit_{brake_count}.png"
+                file_name = f"Lap_{lap}_Brake hit_{brake_count}.png"
                 mss.tools.to_png(sct_img.rgb, sct_img.size, output=file_loc)
 
-            print(f"    Screenshot saved as: {file_name}")
+            print(f"    -Braking position captureed as: {file_name}")
 
         was_L2_pressed = l2_pressed
 
+        # R2 screenshot
+        r2_pressed = bool(state.R2)
+        if record_start and r2_pressed and not was_R2_pressed:
+            throttle_count += 1
+            print(f"R2 press #{throttle_count}")
+
+            with mss.mss() as sct:
+                monitor = sct.monitors[1]
+                sct_img = sct.grab(monitor)
+
+                file_loc = f"../Data/image-data/throttle/Lap_{lap}_Throttle hit_{throttle_count}.png"
+                file_name = f"Lap_{lap}_Throttle hit_{throttle_count}.png"
+                mss.tools.to_png(sct_img.rgb, sct_img.size, output=file_loc)
+
+            print(f"    -Throttle position captured as: {file_name}")
+
+        was_R2_pressed = r2_pressed
+
         if state.share:
-            print("Share pressed, exiting")
+            print("Share pressed, exiting...")
             break
 
         time.sleep(0.01)
 
 finally:
     ds.close()
+    
