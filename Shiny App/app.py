47abeb5c04b2d1b7e8d6a7955a7861e791ae07fb
Prachet Singh <prachetsingh06@gmail.com>
2026-01-20 07:41:10 +1100

    Basic page layout for the shiny app.

--- Shiny App/app.py
+++ Shiny App/app.py
@@ -1,2 +1,12 @@
 # TODO: Make an app that uses your CNN and perhaps other regression models etc.
+from shiny import reactive, req
+from shiny.express import input, ui
+from shinywidgets import render_plotly
 
+ui.page_opts(title="F1", fillable=True)
+ui.include_css("../Shiny App/styles.css")
+
+with ui.sidebar():
+    "Sidebar (input)"
+
+"Main content (output)"
